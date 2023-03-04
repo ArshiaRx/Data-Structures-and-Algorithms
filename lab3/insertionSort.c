c00b64cde390620bcfdaa5655e213d5ab242330a
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2023-03-04 01:37:04 -0500

    Update insertionSort.c

--- lab3/insertionSort.c
+++ lab3/insertionSort.c
@@ -12,7 +12,7 @@ void mySort(int array[], unsigned int first, unsigned int last){
     for (int j = first + 1; j <= last; j++){
         //key = array[i];
         myCopy(&array[i], &key);     //copy value of array[i] to key
-        i = j - 1;
+        int i = j - 1;
 
                         //array[i] > key
         while(i >= 0 && myCompare(array[i], key) >= 0){
