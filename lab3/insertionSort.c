3d9bf4dca46d9afa0fa9f173b897d441c884b48b
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2023-03-04 01:36:37 -0500

    Update insertionSort.c

--- lab3/insertionSort.c
+++ lab3/insertionSort.c
@@ -2,38 +2,23 @@
 #include "metrics.h"
 
 //InsertionSort
-//Procedure:(
+//Procedure:
 //      - Starts at the first index keep checking the next value if greater than current then swap the value
 //        and set them in an ascending order
 
 void mySort(int array[], unsigned int first, unsigned int last){
-    int i, key, j;
+    int key;
 
-    for (i = 1; i <= last; i++){
+    for (int j = first + 1; j <= last; j++){
+        //key = array[i];
         myCopy(&array[i], &key);     //copy value of array[i] to key
-        j = i - 1;
+        i = j - 1;
 
-        while(j >= 0 && myCompare(array[j], key) >= 0){
-            mySwap(&array[j], &array[j + 1]);
-            j--;
+                        //array[i] > key
+        while(i >= 0 && myCompare(array[i], key) >= 0){
+            mySwap(&array[i], &array[i + 1]);
+            i--;      //i = i - 1
         }
+        array[i + 1] = key;
     }
 }
-    /*{
-        //InsertionSort
-
-        int key, i;
- 
-        for (int j = 2; j < last + 1; j++){
-            myCopy(&array[j], &key);
-            //key = array[j];
-            i = j - 1;
-            
-            while (i > 0 && myCompare(array[i], key) >= 0){
-                mySwap(&array[i], &array[i+1]);
-                i = i - 1;
-            }
-            //array[i+1] = key;
-        }
-    }
-*/
